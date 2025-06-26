9b4021ab1a32da5e66dc63983739f369d1f1f32e
malsarraf98 <maximilian.alsarraf@gmail.com>
2025-06-26 11:19:13 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Welcome to AgentGPT Prototype</h1>
-      <p className="text-lg">Use the sidebar to access features like transcript summaries and comp tools.</p>
-    </div>
-  );
-}
