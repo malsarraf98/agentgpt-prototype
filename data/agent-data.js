a822bdde5ec5bce350cc6172eed69a6439c60f6d
malsarraf98 <maximilian.alsarraf@gmail.com>
2025-07-02 11:04:52 -0400

    Create agent-data.js

---
+++ data/agent-data.js
@@ -0,0 +1,44 @@
+const agentDeals = [
+  {
+    client: "John & Sarah",
+    type: "Buy",
+    town: "Newton, MA",
+    price: 1425000,
+    closed: "2024-12-14",
+    daysOnMarket: 11,
+    overAsk: true,
+    repeatClient: false
+  },
+  {
+    client: "Michael Tran",
+    type: "Sell",
+    town: "Brookline, MA",
+    price: 1980000,
+    closed: "2025-02-03",
+    daysOnMarket: 8,
+    overAsk: true,
+    repeatClient: true
+  },
+  {
+    client: "Emily Santos",
+    type: "Buy",
+    town: "Cambridge, MA",
+    price: 1230000,
+    closed: "2025-04-10",
+    daysOnMarket: 16,
+    overAsk: false,
+    repeatClient: false
+  },
+  {
+    client: "Paul & Lisa",
+    type: "Sell",
+    town: "Newton, MA",
+    price: 1540000,
+    closed: "2025-05-27",
+    daysOnMarket: 6,
+    overAsk: true,
+    repeatClient: true
+  }
+];
+
+export default agentDeals;
